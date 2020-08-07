07ab62e1a7e4d111dd86b5316e3da06a6a40f081
Marco Mansi <marco@olandese.nl>
2020-08-07 10:43:26 +0200

    Update CustomScript.ps1

--- CustomScript.ps1
+++ CustomScript.ps1
@@ -2,18 +2,11 @@
 #remove-item 'C:\inetpub\wwwroot\iisstart.htm' -confirm:$false -Force
 #Add-Content -Path 'C:\inetpub\wwwroot\iisstart.htm' -Value $('Hello World from ' + $env:computername)
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\Docker\docker-compose.exe
 docker pull olandese/chatapplication
 docker pull olandese/rabbit-qs2
-
-#$url = "https://raw.githubusercontent.com/olandese/test/master/docker-compose.yml"
-#$output = "docker-compose.yml"
-#Invoke-WebRequest -Uri $url -OutFile $output
-
-#$url = "https://raw.githubusercontent.com/olandese/test/master/.env"
-#$output = ".env"
-#Invoke-WebRequest -Uri $url -OutFile $output
 
 cd c:\POC\docker
 
